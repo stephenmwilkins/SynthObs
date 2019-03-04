27adc9aa07a14b2796ede01c0f44b4ac1482df9b
stephenmwilkins <s.wilkins@sussex.com>
2019-03-04 08:48:17 +0000

    improved examples

--- examples/test_rest_images.py
+++ examples/test_rest_images.py
@@ -24,36 +24,52 @@ from SynthObs.Morph import measure
 model = models.define_model('BPASSv2.2.1.binary/ModSalpeter_300') # DEFINE SED GRID - 
 model.dust = {'A': 5.2, 'slope': -1.0} # DEFINE DUST MODEL - these are the calibrated z=8 values for the dust model
 
+intrinsic_model = models.define_model('BPASSv2.2.1.binary/ModSalpeter_300') # DEFINE SED GRID - 
+intrinsic_model.dust = False # DEFINE DUST MODEL - these are the calibrated z=8 values for the dust model
+
+
 
 filters = FLARE.filters.FAKE
+
+# filters = ['FAKE.FAKE.'+f for f in ['1500','Hth']]
 
 F = FLARE.filters.add_filters(filters, new_lam = model.lam) 
 
 
 model.create_Lnu_grid(F)
-
+intrinsic_model.create_Lnu_grid(F)
 
 test = SynthObs.test_data() # --- read in some test data
 
 L = {f: models.generate_Lnu_array(model, test.Masses, test.Ages, test.Metallicities, test.MetSurfaceDensities, F, f) for f in filters}
 
+L.update({'intrinsic_'+f: models.generate_Lnu_array(intrinsic_model, test.Masses, test.Ages, test.Metallicities, test.MetSurfaceDensities, F, f) for f in filters})
+
+
 
 # ------ Make rest-frame luminosity image
 
-IMGs = SynthObs.Morph.physical_images(test.X, test.Y, L, filters, Ndim = 50)
+IMGs = SynthObs.Morph.physical_images(test.X, test.Y, L, L.keys(), Ndim = 50)
 
-fig, axes = plt.subplots(1, len(filters), figsize = (len(filters)*2., 2))
+fig, axes = plt.subplots(2, len(filters), figsize = (len(filters)*2., 4))
 fig.subplots_adjust(left=0.0, bottom=0.0, right=1.0, top=1.0, wspace=0.0, hspace=0.0)
 
-for ax, f in zip(axes.flatten(), filters):   
-    ax.imshow(IMGs[f].img)  
-    ax.get_xaxis().set_ticks([])
-    ax.get_yaxis().set_ticks([])
-    ax.text(0.5, 0.9, f.split('.')[-1], fontsize = 30, color='1.0', alpha = 0.3, horizontalalignment='center', verticalalignment='center', transform=ax.transAxes)
+
+for i, f in enumerate(filters):
+   
+    axes[0,i].imshow(IMGs[f].img)  
+    axes[0,i].get_xaxis().set_ticks([])
+    axes[0,i].get_yaxis().set_ticks([])
+    
+    axes[1,i].imshow(IMGs['intrinsic_'+f].img)  
+    axes[1,i].get_xaxis().set_ticks([])
+    axes[1,i].get_yaxis().set_ticks([])
+    
+    axes[0,i].text(0.5, 0.9, f.split('.')[-1], fontsize = 30, color='1.0', alpha = 0.3, horizontalalignment='center', verticalalignment='center', transform=axes[0,i].transAxes)
 
 
 fig.savefig('rest.png')
-# plt.show()
+plt.show()
 
 
 
