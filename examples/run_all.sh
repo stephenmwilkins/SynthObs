d1583b1f66a062183abe61e9a1d71292ef9d54f2
stephenmwilkins <s.wilkins@sussex.com>
2019-03-05 14:51:50 +0000

    added script to run all examples

---
+++ examples/run_all.sh
@@ -0,0 +1,11 @@
+python core_manytestobjects.py
+
+python SED_spectra.py
+python SED_luminosities.py
+python SED_fluxes.py
+
+python morph_image_physical.py
+python morph_image_rest.py
+python morph_image_webb.py
+python morph_measure_physical.py
+python morph_webbpsf.py
