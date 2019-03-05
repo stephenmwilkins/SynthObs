8aa1fdd6b64f0776034bf9ad9c4c1ebba0946232
stephenmwilkins <s.wilkins@sussex.com>
2019-03-05 07:41:06 +0000

    changes to image normalisation

--- examples/test_rest_images.py
+++ examples/test_rest_images.py
@@ -49,21 +49,35 @@ L.update({'intrinsic_'+f: models.generate_Lnu_array(intrinsic_model, test.Masses
 
 # ------ Make rest-frame luminosity image
 
-IMGs = SynthObs.Morph.physical_images(test.X, test.Y, L, L.keys(), Ndim = 50)
+IMGs = SynthObs.Morph.physical_images(test.X, test.Y, L, L.keys(), Ndim = 100)
+
+
+
 
 fig, axes = plt.subplots(2, len(filters), figsize = (len(filters)*2., 4))
 fig.subplots_adjust(left=0.0, bottom=0.0, right=1.0, top=1.0, wspace=0.0, hspace=0.0)
 
 
+mx = np.max([np.max(IMGs[f].img) for f in filters])
+mx_intrinsic = np.max([np.max(IMGs['intrinsic_'+f].img) for f in filters])
+
+print(mx,mx_intrinsic)
+
+
 for i, f in enumerate(filters):
    
-    axes[0,i].imshow(IMGs[f].img)  
+   
+   
+    axes[0,i].imshow(IMGs[f].img, vmin = 0.0, vmax = mx)  
     axes[0,i].get_xaxis().set_ticks([])
     axes[0,i].get_yaxis().set_ticks([])
     
-    axes[1,i].imshow(IMGs['intrinsic_'+f].img)  
+    axes[1,i].imshow(IMGs['intrinsic_'+f].img, vmin = 0.0, vmax = mx_intrinsic)  
     axes[1,i].get_xaxis().set_ticks([])
     axes[1,i].get_yaxis().set_ticks([])
+   
+   
+    print(f, np.sum(IMGs['intrinsic_'+f].img), np.sum(IMGs[f].img))
     
     axes[0,i].text(0.5, 0.9, f.split('.')[-1], fontsize = 30, color='1.0', alpha = 0.3, horizontalalignment='center', verticalalignment='center', transform=axes[0,i].transAxes)
 
